a49d1cfc26a077e65c78a9a7dec81e1ae6ec2cd1
Emmanuel Cobbinah <emmanuelcobbinah707@gmail.com>
2024-10-21 21:40:45 +0000

    Started working on the form in expenses

--- src/pages/Expenses.jsx
+++ src/pages/Expenses.jsx
@@ -2,7 +2,26 @@ import React from 'react'
 
 const Expenses = () => {
   return (
-    <div>Expenses</div>
+    <div>
+      <div>
+
+      </div>
+      <div>
+        <h1>Add Expense</h1>
+        <div>
+        <form>
+          <input className='bg-white block' type="text" placeholder='Description' />
+          <input className='bg-white vlock' type="number" placeholder='Amount' />
+          <p className='inline'>Category:</p>
+          <select name="Category" id="">
+            <option value="utilities">Utilities</option>
+            <option value="food">Food</option>
+            <option value="mics">Miscellenous</option>
+          </select>
+        </form>
+        </div>
+      </div>
+    </div>
   )
 }
 
