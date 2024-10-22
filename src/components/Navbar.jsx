406b7e9077142d4eece6acecf8f8092762861a2b
Emmanuel Cobbinah <emmanuelcobbinah707@gmail.com>
2024-10-22 08:16:13 +0000

    Made the Expense form look pretty

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,13 +5,13 @@ import DefaultUser from '../assets/DefaultUser.png'
 const Navbar = () => {
     const linkClass = ({ isActive }) =>
         isActive
-          ? 'px-4 hover:font-bold duration-300 font-bold text-md'
+          ? 'px-4 font-bold text-md '
           : 'px-4 hover:font-bold duration-300';
 
   return (
     <div>
         <div className='h-24 max-w-[1240px] mx-auto flex justify-between items-center'>
-            <h1 className='font-bold text-3xl text-[#009688]'><Link to='/'>Hello Finance!</Link></h1>
+            <h1 className='font-bold text-3xl text-[#009688] px-6'><Link to='/'>Hello Finance!</Link></h1>
             <div className=''>
                 <ul className='flex text-md'>
                     <NavLink className={linkClass} to='/'><li>Dashboard</li></NavLink>
--- src/pages/Expenses.jsx
+++ src/pages/Expenses.jsx
@@ -2,22 +2,23 @@ import React from 'react'
 
 const Expenses = () => {
   return (
-    <div>
-      <div>
+    <div className='grid grid-cols-3 max-w-[1240px] mx-auto'>
+      <div className='col-span-2 border-r border-r-gray-200 h-screen p-6'>
 
       </div>
-      <div>
-        <h1>Add Expense</h1>
+      <div className='flex flex-col my-8 pr-4 pl-8'>
+        <h1 className='font-bold text-xl pt-3 pb-3 text-center text-[#009688]'>Add Expense</h1>
         <div>
         <form>
-          <input className='bg-white block' type="text" placeholder='Description' />
-          <input className='bg-white vlock' type="number" placeholder='Amount' />
-          <p className='inline'>Category:</p>
-          <select name="Category" id="">
+          <input className='rounded-md p-3 w-full text-black bg-white block my-3' type="text" placeholder='Description' />
+          <input className='rounded-md p-3 w-full text-black bg-white block my-5' type="number" placeholder='Amount (GHS)' />
+          <label for='category'  className='inline pr-4 m-10 font-bold'>Category:</label>
+          <select id='category' name="Category" >
             <option value="utilities">Utilities</option>
             <option value="food">Food</option>
             <option value="mics">Miscellenous</option>
           </select>
+          <button className='block w-[250px] bg-[#263238] text-[#F5F5F5] text-lg rounded-lg mx-auto my-6 py-1 font-bold hover:bg-[#009688] duration-300'>Add+</button>
         </form>
         </div>
       </div>
